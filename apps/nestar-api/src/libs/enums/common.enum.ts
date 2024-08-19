babff59bd93365ff086685b4f9b73d40ca7c8a61
RODI-099 <rodiiy099@gmail.com>
2024-08-19 19:43:30 +0500

    feat: built project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,19 @@
+export enum Message {
+    SOMETHING_WENT_WRONG = "Something wen wrong!",
+    CREATE_FAILED = "Create Failed!",
+    UPDATE_FAILED = "Update failed!",
+    REMOVE_FAILED = "Remove failed!",
+    UPLOAD_FAILED = "Upload failed!",
+    BAD_REQUEST = "Bad request!",
+
+    NO_MEMBER_NICK = "No membewr with that member nick",
+    BLOCKED_USER = " You have been blocked",
+    WRONG_PASSWORD = " Wrong paswword, try again!",
+    NOT_AUTHENTICATED = "You are not authenticated, please login first!",
+    TOKEN_NOT_EXIST = " Bearer Token is not provides!",
+    ONLY_SPECIFIC_ROLES_ALLOWED = "Allowed only for members with specific roles!",
+    NOT_ALLOWED_REQUEST = "Not allowed request!",
+    PROVIDE_ALLOWED_FORMAT = " Please provide jpg, jpeg, or png images!",
+    SELF_SUBSCRIPTION_DENIED = "Self sunscription is denied!",
+
+}
