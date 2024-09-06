1c86d688e51b9f62f696d2af92e88f9f8ff41233
RODI-099 <rodiiy099@gmail.com>
2024-09-06 18:16:01 +0500

    feat: get properties graphQl api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,11 +1,16 @@
-import { Query, Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
-import { Property } from '../../libs/dto/property/property';
-import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Properties, Property } from '../../libs/dto/property/property';
+import {
+	AgentsPropertiesInquiry,
+	AllPropertiesInquiry,
+	PropertiesInquiry,
+	PropertysInput,
+} from '../../libs/dto/property/property.input';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
-import { RolesGuard } from '../auth/guards/roles.guard';
 import {  UseGuards } from '@nestjs/common';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
@@ -14,37 +19,91 @@ import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
 export class PropertyResolver {
-    constructor(private readonly propertyService: PropertyService) {} 
-    
-    @Roles(MemberType.AGENT)
-    @UseGuards(RolesGuard)
-    @Mutation(() => Property)
-    public async createProperty(@Args('input') input:  PropertyInput, @AuthMember("_id") memberId: ObjectId): Promise<Property> {
-        console.log("Mutation: createProperty");
-        input.memberId = memberId;
-        return await this.propertyService.createProperty(input);
-    }
+	constructor(private readonly propertyService: PropertyService) {}
 
-    @UseGuards(WithoutGuard)
-    @Query((returns) => Property)
-    public async getProperty(
-        @Args('propertyId') input: string,
-        @AuthMember('_id') memberId: ObjectId,
-    ): Promise<Property>{
-        console.log("Query: getProperty");
-        const propertyId = shapeIntoMongoObjectId(input);
-        return await this.propertyService.getProperty(memberId, propertyId)
-    }
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Property)
+	public async createProperty(
+		@Args('input') input: PropertysInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation: createProperty');
+		input.memberId = memberId;
+		return await this.propertyService.createProperty(input);
+	}
 
-    @Roles(MemberType.AGENT)
-    @UseGuards(RolesGuard)
-    @Mutation((returns) => Property)
-    public async updateProperty(
-        @Args('input') input: PropertyUpdate,
-        @AuthMember('_id') memberId: ObjectId,
-    ): Promise<Property> {
-        console.log('Mutation: updateProperty');
-        input._id = shapeIntoMongoObjectId(input._id);
-        return await this.propertyService.updateProperty(memberId, input)
-    }
+	@UseGuards(WithoutGuard)
+	@Query(() => Property)
+	public async getProperty(
+		@Args('propertyId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Query: getProperty');
+		const propertyId = shapeIntoMongoObjectId(input);
+		return await this.propertyService.getProperty(memberId, propertyId);
+	}
+
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async updateProperty(
+		@Args('input') input: PropertyUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation: updateProperty');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.propertyService.updateProperty(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Properties)
+	public async getProperties(
+		@Args('input') input: PropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getProperties');
+		return await this.propertyService.getProperties(memberId, input);
+	}
+
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Query((returns) => Properties)
+	public async getAgentProperties(
+		@Args('input') input: AgentsPropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getAgentProperties');
+		return await this.propertyService.getAgentProperties(memberId, input);
+	}
+
+	/** ADMIN */
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Query((returns) => Properties)
+	public async getAllPropertiesByAdmin(
+		@Args('input') input: AllPropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getAllPropertiesByAdmin');
+		return await this.propertyService.getAllPropertiesByAdmin(input);
+	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async updatePropertyByAdmin(@Args('input') input: PropertyUpdate): Promise<Property> {
+		console.log('Mutataion: updatePropertyByAdmin');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.propertyService.updatePropertyByAdmin(input);
+	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async removePropertyByAdmin(@Args('propertyId') input: string): Promise<Property> {
+		console.log('Mutattion: removePropertyByAdmin');
+		const propertyId = shapeIntoMongoObjectId(input);
+		return await this.propertyService.removePropertyByAdmin(propertyId);
+	}
 }
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,20 +1,36 @@
-import { ObjectId } from "bson"
 
-export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank" ];
-export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews" ]
+export const availableAgentSorts = ['createdAt', 'updtaedAt', 'memberLikes', 'memberViews', 'memberRank'];
+export const availableMemberSorts = ['createdAt', 'updtaedAt', 'memberLikes', 'memberViews'];
 
- // IMAGE CONFIGURATION (config.js)
- import { v4 as uuidv4 } from 'uuid';
- import * as path from 'path';
- 
- export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
- export const getSerialForImage = (filename: string) => {
-     const ext = path.parse(filename).ext;
-     return uuidv4() + ext;
- };
- 
+export const availabeOptions = ['propertyBarter', 'propertyRent'];
+export const availablePropertySorts = [
+	'createdAt',
+	'updtedAt',
+	'propertyLikes',
+	'propertyviews',
+	'propertyRank',
+	'propertyPrice',
+];
+
+/** IMAGE CONFIGURATION  */
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
+
 export const shapeIntoMongoObjectId = (target: any) => {
-    return typeof target === "string" ? new ObjectId(target) : target;
+	return typeof target === 'string' ? new Object(target) : target;
+};
 
-
-}
+export const lookupMember = {
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
+};
