907b984fe635301732aa497b7015d47573f79738
RODI-099 <rodiiy099@gmail.com>
2024-09-10 17:48:51 +0500

    fix: comment related api

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -1,16 +1,16 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
-import { UseGuards } from '@nestjs/common';
+import { InternalServerErrorException, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
-import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
-import { ObjectId } from 'mongoose';
+import { Comments, Comment } from '../../libs/dto/comment/comment';
+import { ObjectId} from 'mongoose';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
-import { shapeIntoMongoObjectId } from '../../libs/config';
 import { WithoutGuard } from '../auth/guards/without.guard';
-import { MemberType } from '../../libs/enums/member.enum';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
@@ -18,44 +18,44 @@ export class CommentResolver {
 	constructor(private readonly commentService: CommentService) {}
 
 	@UseGuards(AuthGuard)
-	@Mutation((returns) => Comment)
+	@Mutation(() => Comment)
 	public async createComment(
 		@Args('input') input: CommentInput,
 		@AuthMember('_id') memberId: ObjectId,
 	): Promise<Comment> {
-		console.log('Mutation: createComment');
+		console.log(`Mutation createComment`);
 		return await this.commentService.createComment(memberId, input);
 	}
 
-	@UseGuards(AuthGuard)
-	@Mutation((returns) => Comment)
-	public async updateComment(
+    @UseGuards(AuthGuard)
+	@Mutation(() => Comment)
+    public async updateComment(
 		@Args('input') input: CommentUpdate,
 		@AuthMember('_id') memberId: ObjectId,
 	): Promise<Comment> {
-		console.log('Mutation: updateComment');
-		input._id = shapeIntoMongoObjectId(input._id);
+		console.log(`Mutation updateComment`);
+        
 		return await this.commentService.updateComment(memberId, input);
 	}
 
-	@UseGuards(WithoutGuard)
-	@Query((returns) => Comments)
+    @UseGuards(WithoutGuard)
+	@Query(() => Comments)
 	public async getComments(
 		@Args('input') input: CommentsInquiry,
 		@AuthMember('_id') memberId: ObjectId,
 	): Promise<Comments> {
-		console.log('Query: getComments');
+		console.log(`Mutation getComments`);
 		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
 		return await this.commentService.getComments(memberId, input);
 	}
 
-	/** ADMIN */
-	@Roles(MemberType.ADMIN)
-	@UseGuards(RolesGuard)
-	@Mutation((returns) => Comment)
-	public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
-		console.log('Mutattion: removeCommentByAdmin');
-		const commentId = shapeIntoMongoObjectId(input);
-		return await this.commentService.removeCommentByAdmin(commentId);
-	}
+    /** ADMIN **/
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation((returns) => Comment)
+    public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment>{
+        console.log(`Mutation: removeCommentByAdmin`);
+        const commentId = shapeIntoMongoObjectId(input);
+        return await this.commentService.removeCommentByAdmin(commentId)
+    }
 }
--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Comment, Comments } from '../../libs/dto/comment/comment';
-import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
+import { ViewService } from '../view/view.service';
 import { PropertyService } from '../property/property.service';
 import { BoardArticleService } from '../board-article/board-article.service';
+import { Model, ObjectId } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
 import { Direction, Message } from '../../libs/enums/common.enum';
+import { Comments, Comment } from '../../libs/dto/comment/comment';
 import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 import { T } from '../../libs/types/common';
@@ -14,21 +15,22 @@ import { lookupMember } from '../../libs/config';
 
 @Injectable()
 export class CommentService {
-	constructor(
+    constructor(
 		@InjectModel('Comment') private readonly commentModel: Model<Comment>,
 		private readonly memberService: MemberService,
 		private readonly propertyService: PropertyService,
-		private readonly boardArticleService: BoardArticleService,
+        private readonly boardArticleService: BoardArticleService,
 	) {}
 
-	public async createComment(memberId: ObjectId, input: CommentInput): Promise<Comment> {
-		input.memberId = memberId;
-		let result = null;
 
+    public async createComment(memberId: ObjectId, input: CommentInput): Promise<Comment> {
+		input.memberId = memberId;
+
+		let result = null;
 		try {
 			result = await this.commentModel.create(input);
 		} catch (err) {
-			console.log('Error on createComment', err.message);
+			console.log('Error, Service.model:', err.message);
 			throw new BadRequestException(Message.CREATE_FAILED);
 		}
 
@@ -47,7 +49,6 @@ export class CommentService {
 					modifier: 1,
 				});
 				break;
-
 			case CommentGroup.MEMBER:
 				await this.memberService.memberStatsEditor({
 					_id: input.commentRefId,
@@ -58,13 +59,11 @@ export class CommentService {
 		}
 
 		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
-
 		return result;
 	}
 
-	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
+    public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
 		const { _id } = input;
-
 		const result = await this.commentModel.findOneAndUpdate(
 			{
 				_id: _id,
@@ -78,34 +77,33 @@ export class CommentService {
 		return result;
 	}
 
-	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+    public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
 		const { commentRefId } = input.search;
-		const match: T = { commentRefId: commentRefId, CommentStatus: CommentStatus.ACTIVE };
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
 		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-		const result: Comments[] = await this.commentModel
-			.aggregate([
-				{ $match: match },
-				{ $sort: sort },
-				{
-					$facet: {
-						list: [
-							{ $skip: (input.page - 1) * input.limit },
-							{ $limit: input.limit },
-							lookupMember,
-							{ $unwind: '$memberData' },
-						],
-						metaCounter: [{ $count: 'total' }],
-					},
+		const result: Comments[] = await this.commentModel.aggregate([
+			{ $match: match },
+			{ $sort: sort },
+			{
+				$facet: {
+					list: [
+						{ $skip: (input.page - 1) * input.limit },
+						{ $limit: input.limit },
+						//meLiked
+						lookupMember,
+						{ $unwind: '$memberData' },
+					],
+					metaCounter: [{ $count: 'total' }],
 				},
-			])
-			.exec();
+			},
+		]);
 		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
 		return result[0];
 	}
 
-	public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+    public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
 		const result = await this.commentModel.findByIdAndDelete(input);
 		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
 		return result;
